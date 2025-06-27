c3e5e1933617db704b72b0791e70ccd9cd896f0c
dev-hyeonmin <dev.hyeonmin@gmail.com>
2025-06-27 15:29:30 +0900

    style: change pc nav contact color

--- src/components/layout/menu/PCNavigation.tsx
+++ src/components/layout/menu/PCNavigation.tsx
@@ -4,7 +4,7 @@ export default function PCNavigation() {
   return (
     <div className="hidden md:block">
       <div className="flex">
-        <Menu className="px-4 py-3 text-xl font-medium last:ml-4 last:rounded-full last:bg-black last:px-10 last:text-white" />
+        <Menu className="px-4 py-3 text-xl font-medium last:ml-4 last:rounded-full last:bg-zinc-200 last:px-10" />
       </div>
     </div>
   );
