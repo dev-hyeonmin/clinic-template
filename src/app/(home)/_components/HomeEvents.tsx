b221bf2440499a9448cb7ebee1b421d7f3344f43
dev-hyeonmin <dev.hyeonmin@gmail.com>
2025-06-30 15:20:00 +0900

    chore: add home section

--- src/app/(home)/_components/HomeEvents.tsx
+++ src/app/(home)/_components/HomeEvents.tsx
@@ -3,7 +3,7 @@
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function HomeEventPreview() {
+export default function HomeEvents() {
   return (
     <div className="flex gap-4 py-12 pr-0 pl-4 md:px-20 md:py-20">
       <Swiper
@@ -21,8 +21,8 @@ export default function HomeEventPreview() {
         {[...Array(3)].map((_, index) => (
           <SwiperSlide key={index} className="relative h-full w-full">
             <div>
-              <div className="aspect-4/5 w-full rounded-3xl bg-zinc-200" />
-              <div className="mt-4 pl-1">
+              <div className="aspect-4/5 w-full rounded-xl bg-zinc-200" />
+              <div className="mt-4">
                 <p className="text-base font-medium md:text-xl">SUMMER EVENT</p>
                 <p className="mt-1 text-sm text-zinc-700 md:text-lg">
                   6~8월 여름 시즌 전용 색소·여드름·지방분해·보톡스 관리 프로그램. 슈링크 유니버스
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import HomeHero from '@/app/(home)/_components/HomeHero';
 import HomeEventPreview from '@/app/(home)/_components/HomeEventPreview';
 import HomeEventSeeMore from '@/app/(home)/_components/HomeEventSeeMore';
+import HomeClinicOverView from '@/app/(home)/_components/HomeClinicOverView';
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <HomeHero />
       <HomeEventPreview />
       <HomeEventSeeMore />
+      <HomeClinicOverView />
     </>
   );
 }
